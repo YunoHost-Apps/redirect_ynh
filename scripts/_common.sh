2684e5142745ae5a7baf004db47b21e53b067637
Kayou <pierre@kayou.io>
2024-01-02 23:05:32 +0100

    Just adding some getops

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,13 +5,13 @@ URL_REGEX_SECURE='^(http://(127\.[0-9]+\.[0-9]+\.[0-9]+|localhost)|https://.*)(:
 
 _validate_redirect_uri() {
     if [[ ! $target =~ $URL_REGEX_VALID ]]; then
-        ynh_die "Invalid destination: $target" 1
+        ynh_die --message="Invalid destination: $target" 1
     fi
 
     # Avoid uncrypted remote destination with reverse proxy mode
     # Indeed the SSO send the password in all requests in HTTP headers
     if [[ "$redirect_type" = "reverseproxy" ]] && [[ ! $target =~ $URL_REGEX_SECURE ]]; then
-        ynh_die "For secure reason, you can't use an unencrypted http remote destination couple with ssowat for your reverse proxy: $target" 1
+        ynh_die --message="For secure reason, you can't use an unencrypted http remote destination couple with ssowat for your reverse proxy: $target" 1
     fi
 }
 
